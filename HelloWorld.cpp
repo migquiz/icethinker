7b7bf719e68e1be9458dc8097970a5db84218e68
Miguel Quizon <quizon.mig@gmail.com>
2017-04-03 13:26:27 +0800

    Added HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+//Just like what the tradition wants
+//Hello World at it's best!
+
+/*************************
+* Author : Miguel Quizon *
+**************************/
+
+#include <iostream>
+using namespace std;
+
+int main (int argc, char ** argv) {
+	cout << "Hello World!" << endl;
+	return 0;
+}
