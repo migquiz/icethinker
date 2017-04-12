a4b6b780ebc1f4a7d79edd23ea35c55fc2079c90
Miguel Quizon <quizon.mig@gmail.com>
2017-04-12 16:06:47 +0800

    Added BalnkLines code

---
+++ BlankLines.cpp
@@ -0,0 +1,29 @@
+//Actually this source code is not on the book
+//This program is used to output lines of blank characters
+//for renaming the icons on the desktop
+
+/*************************
+* Author : Miguel Quizon *
+**************************/
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main (int argc, char ** argv) {
+
+	const char * filename = "shortcut.txt";
+	const char * blank = "    ";
+
+	FILE * file_write = fopen(filename,"w");
+
+	for (int y = 0; y <= 13 ; ++y) {
+		for (int x = 0; x <= y; ++x) {
+			fputs(" ",file_write);
+		}
+		fputs("\n", file_write);
+	}
+
+	fclose(file_write);
+	return 0;
+}
