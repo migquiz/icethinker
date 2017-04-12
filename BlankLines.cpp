74d86e9eb7b0dd6a7160fc12bccd5c4a700fc672
Miguel Quizon <quizon.mig@gmail.com>
2017-04-12 16:09:29 +0800

    Added comments on the code

--- BlankLines.cpp
+++ BlankLines.cpp
@@ -15,15 +15,15 @@ int main (int argc, char ** argv) {
 	const char * filename = "shortcut.txt";
 	const char * blank = "    ";
 
-	FILE * file_write = fopen(filename,"w");
+	FILE * file_write = fopen(filename,"w"); //create and open the file
 
 	for (int y = 0; y <= 13 ; ++y) {
 		for (int x = 0; x <= y; ++x) {
-			fputs(" ",file_write);
+			fputs(" ",file_write); //write something on the file
 		}
 		fputs("\n", file_write);
 	}
 
-	fclose(file_write);
+	fclose(file_write); //closes the file
 	return 0;
 }
