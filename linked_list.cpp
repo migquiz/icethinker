bcb8e77baafa0e5203b7d7b73cacf3ea33ad67a6
Miguel Quizon <quizon.mig@gmail.com>
2017-08-10 23:17:35 +0800

    Added a snip code from a cs50 lecture

---
+++ linked_list.cpp
@@ -0,0 +1,30 @@
+//CS50 Week 5 Data Structures
+//This contains short source code from the lecture
+
+#include <iostream>
+using namespace std;
+
+int main (int argc, char ** argv) {
+	
+	typedef struct node {
+		int num;
+		struct node * next;
+	}
+
+	return 0;
+}
+
+//if im going to declare the primitive data structure 
+//it would be like this
+//int array [10]
+//node new_node;
+//int linked_list 
+
+//inserting
+	//allocate memory
+	//get the pointer of the previos node
+	//assign to new node
+	//assign the pointer of new node to previous node
+
+
+//deleting
--- tracking_state.cpp
+++ tracking_state.cpp
@@ -3,18 +3,22 @@ using namespace std;
 
 int main (int argc, char ** argv) {
 	
-	//char to digit any length
-	cout << "Enter a number : ";
-	char digitChar = cin.get();
-	int number = (digitChar - '0');
-	digitChar = cin.get();
+	char digitChar;
 
-	while (digitChar != 10) {
-		number = number * 10 + (digitChar - '0');
+	do {
+		
 		digitChar = cin.get();
-	}
+		int number = (digitChar - '0');
+		digitChar = cin.get();
 
-	cout << number << endl;
+		while ((digitChar !=  10) && (digitChar != ',')) {
+			number = number * 10 + (digitChar -'0');
+			digitChar = cin.get();
+		}
+
+	} while (digitChar != 10);
+
+	cout << digitChar << endl;
 
 	return 0;
 }
